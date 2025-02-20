82c86af457356d74c06866d05990eef532b7140f
Albin Limani <albinn012@gmail.com>
2025-02-20 16:26:09 +0100

    - Fixed problem with image

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,6 +8,7 @@ import {useState} from "react";
 import animationData from '@/data/confetti.json'
 import MagicButton from "@/components/ui/MagicButton";
 import {IoCopyOutline} from "react-icons/io5";
+import Image from 'next/image'
 
 
 export const BentoGrid = ({
@@ -42,7 +43,7 @@ export const BentoGridItem = ({
     className?: string;
     title?: string | React.ReactNode;
     description?: string | React.ReactNode;
-    img?: string;
+    img: string;
     imgClassName?: string;
     titleClassName?: string;
     spareImg?: string;
@@ -69,9 +70,11 @@ export const BentoGridItem = ({
         >
             <div className={`${id === 6 && 'flex justify-center'} h-full`}>
                 <div className="w-full h-full absolute">
-                    <img src={img}
-                         alt={img}
-                         className={cn(imgClassName, 'object-cover object-center')}
+                    <Image src={img}
+                           alt={img}
+                           width={100}
+                           height={100}
+                           className={cn(imgClassName, 'object-cover object-center')}
                     />
                 </div>
                 <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
@@ -142,11 +145,11 @@ export const BentoGridItem = ({
                                     rendererSettings: {
                                         preserveAspectRatio: 'xMidYMid slice',
                                     }
-                                }} />
+                                }}/>
                             </div>
 
                             <MagicButton title={copied ? 'Email copied' : 'Copy my email'}
-                                         icon={<IoCopyOutline />}
+                                         icon={<IoCopyOutline/>}
                                          position="left"
                                          otherClasses="!bg-[#161a31"
                                          handleClick={handleCopy}
