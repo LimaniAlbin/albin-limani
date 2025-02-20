78304e8ddb7ba718bcb757de5a9a958f45c800f3
Albin Limani <albinn012@gmail.com>
2025-02-20 16:43:22 +0100

    - Fixed problem with image

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -8,7 +8,6 @@ import {useState} from "react";
 import animationData from '@/data/confetti.json'
 import MagicButton from "@/components/ui/MagicButton";
 import {IoCopyOutline} from "react-icons/io5";
-import Image from 'next/image'
 
 
 export const BentoGrid = ({
@@ -70,10 +69,8 @@ export const BentoGridItem = ({
         >
             <div className={`${id === 6 && 'flex justify-center'} h-full`}>
                 <div className="w-full h-full absolute">
-                    <Image src={img}
+                    <img src={img}
                            alt={img}
-                           width={100}
-                           height={100}
                            className={cn(imgClassName, 'object-cover object-center')}
                     />
                 </div>
